f47a373feb9b3505bc7906bb8c082bffeb6031d3
Nas1010 <184663878+Nas1010@users.noreply.github.com>
2025-07-05 20:29:04 +0100

    Add basic expression evaluation with error handling to calculator

---
+++ main.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+from tkinter import ttk
+
+def handle_button_click(clicked_button_text):
+    current_text = result_var.get()
+
+    if clicked_button_text == "=":
+        try:
+
+            expression = current_text.replace("÷", "/").replace("x", "*")
+            result = eval(expression)
+
+            if result.is_integer():
+                result = int(result)
+
+                result_var.set(result)
+        except Exception as e:
+            result_var.set("Error")
